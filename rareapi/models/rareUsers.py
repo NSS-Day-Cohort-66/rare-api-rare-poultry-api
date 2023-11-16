40d6ae90a3fcecd52f5e13af21392ae1102dfa60
Nathan Chevalier <cerberusgfx@gmail.com>
2023-11-16 10:32:42 -0600

    RareUsers model created, data for tokens, users, and RareUsers seeded

---
+++ rareapi/models/rareUsers.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+class RareUsers(models.Model):
+    bio = models.CharField(max_length=2000)
+    profile_image_url = models.URLField(null=True, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
