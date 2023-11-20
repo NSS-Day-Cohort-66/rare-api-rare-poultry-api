e660b8ef497b2c193dbcdaa04744e2dd5702a26b
Nathan Chevalier <cerberusgfx@gmail.com>
2023-11-20 14:55:05 -0600

    Fetch single added

--- rareapi/views/category_view.py
+++ rareapi/views/category_view.py
@@ -14,6 +14,11 @@ class CategoryView(ViewSet):
         categories = Categories.objects.all()
         serialized = CategorySerializer(categories, many=True)
         return Response(serialized.data, status=status.HTTP_200_OK)
+    
+    def retrieve(self, request, pk=None):
+        category = Categories.objects.get(pk=pk)
+        serialized = CategorySerializer(category)
+        return Response(serialized.data, status=status.HTTP_200_OK)
 
 class CategorySerializer(serializers.ModelSerializer):
     """JSON serializer for types"""
