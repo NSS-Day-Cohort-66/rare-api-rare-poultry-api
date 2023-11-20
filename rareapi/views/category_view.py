ac31a63cdb69442ea2adfd71e7778e755d038f20
Nathan Chevalier <cerberusgfx@gmail.com>
2023-11-20 14:57:46 -0600

    POST functioning in categories

--- rareapi/views/category_view.py
+++ rareapi/views/category_view.py
@@ -19,6 +19,14 @@ class CategoryView(ViewSet):
         category = Categories.objects.get(pk=pk)
         serialized = CategorySerializer(category)
         return Response(serialized.data, status=status.HTTP_200_OK)
+    
+    def create(self, request):
+        category = Categories()
+        category.label = request.data['label']
+        category.save()
+
+        serialized = CategorySerializer(category, many=False)
+        return Response(serialized.data, status=status.HTTP_201_CREATED)    
 
 class CategorySerializer(serializers.ModelSerializer):
     """JSON serializer for types"""
