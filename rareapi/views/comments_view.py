d08dcac59c5e9dd967e4c3b5e474a99540ad9ade
Paolo Medel <paolomedel17@gmail.com>
2023-11-20 14:09:06 -0500

    comments view updated to retrieve specific comments tied to a post

--- rareapi/views/comments_view.py
+++ rareapi/views/comments_view.py
@@ -1,9 +1,21 @@
 from rest_framework import viewsets, serializers, status
 from rest_framework.response import Response
-from rareapi.models import Comments
+from rareapi.models import Comments, Posts, RareUsers
+
+
+class CommentAuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RareUsers
+        fields = ('user',)
+
+class CommentPostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Posts
+        fields = ('title',)
 
 class CommentSerializer(serializers.ModelSerializer):
-
+    post = CommentPostSerializer(many=False)
+    author = CommentAuthorSerializer(many=False)
     class Meta:
         model = Comments
         fields = ('id', 'post', 'author', 'content', 'created_on')
@@ -14,3 +26,13 @@ class CommentView(viewsets.ViewSet):
         serialized = CommentSerializer(comments, many=True)
         return Response(serialized.data, status=status.HTTP_200_OK)
     
+    def retrieve(self, request, pk=None):
+        """Handle GET requests for single type
+
+        Returns:
+            Response -- JSON serialized type record
+        """
+
+        comment = Comments.objects.get(pk=pk)
+        serialized = CommentSerializer(comment)
+        return Response(serialized.data, status=status.HTTP_200_OK)
