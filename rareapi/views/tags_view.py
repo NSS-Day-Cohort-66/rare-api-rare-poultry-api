265819547d2e23ec079c8f2c26816805fafbba0c
Nathan Chevalier <cerberusgfx@gmail.com>
2023-11-21 09:35:54 -0600

    PUT request implemented on Tags

--- rareapi/views/tags_view.py
+++ rareapi/views/tags_view.py
@@ -9,16 +9,17 @@ class TagSerializer(serializers.ModelSerializer):
         fields = ('id', 'label',)
 
 class TagView(viewsets.ViewSet):
-    def list(self, request):
+    # List many tags
+    def list(self, request): 
         tags = Tags.objects.all().order_by('label')
         serialized = TagSerializer(tags, many=True)
         return Response(serialized.data, status=status.HTTP_200_OK)
-    
+    # List one tag
     def retrieve(self, request, pk=None):
         tag = Tags.objects.get(pk=pk)
         serialized = TagSerializer(tag)
         return Response(serialized.data, status=status.HTTP_200_OK)
-    
+    # Create a tag
     def create(self, request):
         tag = Tags()
         tag.label = request.data['label']
@@ -26,3 +27,18 @@ class TagView(viewsets.ViewSet):
 
         serialized = TagSerializer(tag, many=False)
         return Response(serialized.data, status=status.HTTP_201_CREATED)
+    # Edit a tag
+    def update(self, request, pk=None):
+        try:
+            tag = Tags.objects.get(pk=pk)
+            serializer = TagSerializer(data=request.data)
+            # Serialize the request data to check validity of the request, if not return an error
+            if serializer.is_valid():
+                tag.label = serializer.validated_data['label']
+                tag.save()
+                serializer = TagSerializer(tag, context={'request': request})
+                return Response(None, status.HTTP_204_NO_CONTENT)
+            return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+        except Tags.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
