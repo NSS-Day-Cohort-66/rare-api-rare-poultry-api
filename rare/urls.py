4544bf38c04f08a27043f481eace382d2cc41ab7
Scott Cole <sscole929@gmail.com>
2023-11-28 09:49:02 -0600

    removed commentsview

--- rare/urls.py
+++ rare/urls.py
@@ -2,12 +2,11 @@ from django.contrib import admin
 from rest_framework.routers import DefaultRouter
 from django.conf.urls import include
 from django.urls import path
-from rareapi.views import register_user, login_user, CategoryView, TagView, CommentView, PostView, RareUsersView
+from rareapi.views import register_user, login_user, CategoryView, TagView, PostView, RareUsersView
 
 router = DefaultRouter(trailing_slash=False)
 router.register(r'categories', CategoryView, 'categories')
 router.register(r'tags', TagView, 'tag')
-router.register(r'comments', CommentView, 'comments')
 router.register(r'posts', PostView, 'posts')
 router.register(r'users', RareUsersView, 'user')
 
