db6bbc765ddcc6db1df7dc4401c6508991e2f926
Nathan Chevalier <cerberusgfx@gmail.com>
2023-11-28 13:46:14 -0600

    Both Rareusers tests working

--- tests/rareusers_tests.py
+++ tests/rareusers_tests.py
@@ -28,29 +28,34 @@ class RareUsersTests(APITestCase):
         self.assertEqual(json_response["profile_image_url"], "https://pbs.twimg.com/profile_images/1279103276055539712/6pcd9iFM_400x400.jpg")
         self.assertEqual(json_response["created_on"], "2023-11-15T18:00:00Z")
         self.assertEqual(json_response["active"], True)
-        self.assertEqual(json_response["rare_username"], "")
+        self.assertEqual(json_response["rare_username"], "Cool Guy")
         self.assertEqual(json_response["user"]["full_name"], "Carrie Belk")
         self.assertEqual(json_response["user"]["email"], "me@me.com")
         self.assertEqual(json_response["user"]["is_staff"], "False")
 
     def test_get_rare_users(self):
-        test_user = User.objects.create_user(
-        username='test@test.com',
-        password='abc123',
-        first_name="Testy",
-        last_name="Testerson"
-    )
-        test_rare_user = RareUsers()
-        test_rare_user.user_id = test_user.id
-        test_rare_user.bio = "Test Bio"
-        test_rare_user.profile_image_url = "www.test.com"
-        test_rare_user.rare_username = "McTester"
-        test_rare_user.save()
+
 
         response = self.client.get("/users")
 
         json_response = json.loads(response.content)
 
-        self.assertEqual
+        self.assertEqual(json_response[0]["bio"], "I'm just like some guy, you know.")
+        self.assertEqual(json_response[0]["profile_image_url"], "https://pbs.twimg.com/profile_images/1279103276055539712/6pcd9iFM_400x400.jpg")
+        self.assertEqual(json_response[0]["created_on"], "2023-11-15T18:00:00Z")
+        self.assertEqual(json_response[0]["active"], True)
+        self.assertEqual(json_response[0]["rare_username"], "Cool Guy")
+        self.assertEqual(json_response[0]["user"]["full_name"], "Carrie Belk")
+        self.assertEqual(json_response[0]["user"]["email"], "me@me.com")
+        self.assertEqual(json_response[0]["user"]["is_staff"], "False")
+
+        self.assertEqual(json_response[1]["bio"], "I'm Pajama Sam")
+        self.assertEqual(json_response[1]["profile_image_url"], "https://pbs.twimg.com/profile_images/1279103276055539712/6pcd9iFM_400x400.jpg")
+        self.assertEqual(json_response[1]["created_on"], "2023-11-10T14:00:00Z")
+        self.assertEqual(json_response[1]["active"], True)
+        self.assertEqual(json_response[1]["rare_username"], "Pajama Sam")
+        self.assertEqual(json_response[1]["user"]["full_name"], "Admina Straytor")
+        self.assertEqual(json_response[1]["user"]["email"], "admina@straytor.com")
+        self.assertEqual(json_response[1]["user"]["is_staff"], "True")
 
 
