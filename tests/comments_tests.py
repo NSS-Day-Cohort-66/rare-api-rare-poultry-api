6550d7e87e7f85ecc13eab1678947baf5db01ada
Nathan Chevalier <cerberusgfx@gmail.com>
2023-11-29 10:26:01 -0600

    Test created for PUT

---
+++ tests/comments_tests.py
@@ -0,0 +1,39 @@
+import json
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from rareapi.models import Comments, RareUsers
+
+class CommentsTests(APITestCase):
+
+    fixtures = ['users', 'rare_users', 'tokens', 'posts', 'comments', 'categories', 'tags']
+
+    def setUp(self):
+        self.user = User.objects.first()
+        self.rare_user = RareUsers.objects.get(user=self.user)
+        token = Token.objects.get(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION=f"Token {token.key}")
+
+    def test_change_comments(self):
+        comment = Comments()
+        comment.post_id = 1
+        comment.author_id = 1
+        comment.content = "Original Comment"
+        comment.save()
+
+        data = {
+            "post": 1,
+            "author": 1,
+            "content": "Edited Comment"
+        }
+
+        response = self.client.put(f"/comments/{comment.id}", data, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        response = self.client.get(f"/comments/{comment.id}")
+        json_response = json.loads(response.content)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(json_response["content"], "Edited Comment")
